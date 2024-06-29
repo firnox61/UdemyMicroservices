e060fd9dd5f83cc06d44889433d525c79fb8c3f7
İSMAİL ENES EROĞLU <73061163+firnox61@users.noreply.github.com>
2024-06-29 10:02:27 +0300

    Proje dosyası ekle.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    internal class Category
+    {
+       // IOptions<DatabaseSett>
+        [BsonId]//ıd olaark mongodb de tanımlansın diye
+        [BsonRepresentation(BsonType.ObjectId)]//tipi
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Shared.Dtos;
+using MongoDB.Driver;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+     interface ICategoryService
+    {
+       
+        Task<Response<List<CategoryDto>>> GetAllAsync();
+        Task<Response<CategoryDto>> CreateAsync(Category category);
+        Task<Response<CategoryDto>> GetByIdAsync(string id);
+
+    }
+}
